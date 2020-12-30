e20cae5bac31a27c6296cfc0eaa7b293b3e9f581
yls <1329926763@qq.com>
2020-12-31 00:33:48 +0800

    update dir name

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ module.exports = {
     devServer: {
         port: 8888
     },
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/ShuDu/'
-        : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/ShuDu/' : '/',
+    outputDir:"ShuDu"
 }
